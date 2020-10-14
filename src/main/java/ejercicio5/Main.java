de77411e01087752b855538c89f5a19dbe4b1972
s0cr4t3sd <smr.jamc@gmail.com>
2020-10-14 10:19:02 +0200

    Ejercicio 5

--- src/main/java/ejercicio5/Main.java
+++ src/main/java/ejercicio5/Main.java
@@ -8,6 +8,7 @@ package ejercicio5;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.File;
+import java.util.ArrayList;
 
 /**
  *
@@ -16,6 +17,8 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         String idFichero = "matriz.txt";
+        ArrayList<Integer> suma = new ArrayList<>();
+        int num, resul=0;
 
         // Variables para guardar los datos que se van leyendo
         String[] tokens;
@@ -27,14 +30,36 @@ public class Main {
         // Estructura try-with-resources. Permite cerrar los recursos una vez finalizadas
         // las operaciones con el archivo
         try (Scanner datosFichero = new Scanner(new File(idFichero))){
-            
-            // Mientras haya líneas por leer
+
+            System.out.println("Separando cada elemento del fichero: ");
+
             while (datosFichero.hasNextLine()) {
 
-                linea = datosFichero.nextLine(); //Se lee la línea
-                System.out.println(linea); // Se imprime en pantalla
-            }
+                linea = datosFichero.nextLine();
 
+                // Se guarda en el array de String cada elemento de la
+                // línea en función del carácter separador coma
+                tokens = linea.split("\t");
+
+                for (int i = 0; i < tokens.length; i++) {
+                    if(!tokens[i].equals("")){
+                        num = Integer.parseInt(tokens[i]);
+                        suma.add(num);
+                    }
+                    
+                }
+                
+                for(String i : tokens){
+                    System.out.print(i+"\t");
+                }
+
+                for (Integer i : suma) {
+                    resul += i;                   
+                } 
+                System.out.println("");
+            }
+            System.out.println("El resultado es: "+resul);
+            
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         } 
